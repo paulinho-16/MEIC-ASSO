8a56f804fe6c82ebb00d7fc1ba2258ab9d3ae0a2
Luís Tavares <luistavares10@outlook.pt>
2022-04-01 11:27:03 +0100

    Add separated endpoints for each restaurant

--- api/src/routes/meals.ts
+++ api/src/routes/meals.ts
@@ -5,5 +5,9 @@ import controller from '@/controller/meals.controller'
 const router = express.Router()
 
 router.get('/canteen', controller.getCanteen)
+router.get('/grill', controller.getGrill)
+router.get('/cafeteria', controller.getCafeteria)
+router.get('/inegi', controller.getInegi)
+router.get('/inesc', controller.getInesc)
 
 export default router
