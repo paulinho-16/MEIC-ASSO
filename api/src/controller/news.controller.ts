413a78fc79e128b12ac0094d61dce09a0af516dd
Francisco Goncalves <kikojpgoncalves@gmail.com>
2022-04-01 15:00:04 +0100

    add: scrape inner news pages

--- api/src/controller/news.controller.ts
+++ api/src/controller/news.controller.ts
@@ -3,36 +3,62 @@ import cheerio from 'cheerio'
 import axios from 'axios'
 
 type News = {
-  url?: string
-  title?: string
-  smallDescription?: string
-  description?: string
+  url: string
+  title: string
+  excerpt: string
+  header?: string
+  details?: string
+  content?: string
 }
+
+const NEWS_URL = 'https://sigarra.up.pt/feup/pt/noticias_geral.lista_noticias'
+const MAIN_URL = 'https://sigarra.up.pt/feup/pt'
+const FALLBACK_URL = 'https://sigarra.up.pt/feup/pt'
 
 async function get(req: Request, res: Response) {
   const news: News[] = []
 
   axios
-    .get('https://sigarra.up.pt/feup/pt/noticias_geral.lista_noticias')
+    .get(NEWS_URL)
     .then(response => {
       const $ = cheerio.load(response.data)
-      const targetUl = $('a[name=gruponot6] ~ ul')[0]
+      const targetUl = $('a[name=gruponot7] ~ ul')[0]
       $(targetUl)
         .find('li')
         .each((i, e) => {
-          const url = $(e).find('span.titulo a').attr('href')
+          const url = $(e).find('span.titulo a').attr('href') || FALLBACK_URL
           const title = $(e).find('span.titulo a').text()
-          const smallDescription = $(e).find('p.textopequenonoticia').text()
+          const excerpt = $(e).find('p.textopequenonoticia').text()
 
           news.push({
-            url: url,
+            url: `${MAIN_URL}/${url}`,
             title: title,
-            smallDescription: smallDescription,
-            description: 'TBD',
+            excerpt: excerpt,
           })
         })
+    })
+    .then(() => {
+      const requests = news.map(({ url }) => axios.get(url))
+      axios
+        .all(requests)
+        .then(
+          axios.spread((...responses) => {
+            responses.forEach((item, index) => {
+              const $ = cheerio.load(item.data)
+              const target = $('#conteudoinner')
+              const header = $(target).first().find('h1').text()
+              const details = $(target).first().find('h2').text()
+              const content = $(target).first().find('p').text()
 
-      res.send(news)
+              news[index].header = header
+              news[index].details = details
+              news[index].content = content
+            })
+          })
+        )
+        .then(() => {
+          res.send(news)
+        })
     })
     .catch(function (e) {
       console.log(e)
