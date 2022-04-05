44341bc184c5480b013275eabab408ac82b96d9f
Ricardo Cardoso <ricardofdc@live.com>
2022-04-05 19:49:31 +0100

    initial version of feedback endpoint

--- api/src/controller/feedback.controller.ts
+++ api/src/controller/feedback.controller.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express'
 
-import function from '@/servives/feddback'
+import feedbackService from '@/services/feedback'
 
 async function get(req: Request, res: Response) {
+  const data = feedbackService.getFeedback()
+  res.send('Feedback route')
+}
+
+async function post(req: Request, res: Response) {
+  const data = feedbackService.sendFeedback()
   res.send('Feedback route')
 }
 
 export default {
   get,
+  post
 }
