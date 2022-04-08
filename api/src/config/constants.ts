ab2b4bee7b6d0eed03993bcd0ca3aee705f572b2
Diana Freitas <up201806230@fe.up.pt>
2022-04-08 14:31:24 +0100

    Fix constants

--- api/src/config/constants.ts
+++ api/src/config/constants.ts
@@ -1,9 +1,5 @@
 export default {
-<<<<<<< HEAD
+  associationsUrl: 'https://paginas.fe.up.pt/~estudar/vida-na-feup/associacoes-de-estudantes/',
   canteenUrl: 'https://sigarra.up.pt/feup/pt/mob_eme_geral.cantinas',
   libraryUrl: 'https://docs.google.com/spreadsheets/d/1gZRbEX4y8vNW7vrl15FCdAQ3pVNRJw_uRZtVL6ORP0g/edit#gid=75324596',
 }
-=======
-    associationsUrl: 'https://paginas.fe.up.pt/~estudar/vida-na-feup/associacoes-de-estudantes/'
-}
->>>>>>> Refactor associations base code structure
