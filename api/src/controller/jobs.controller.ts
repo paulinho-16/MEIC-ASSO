9b531a9c7983be9088b039aad98d2d412a0e70b5
rafaavc <rafavc2508@gmail.com>
2022-04-08 11:54:35 +0100

    Finished initial draft of jobs endpoint

--- api/src/controller/jobs.controller.ts
+++ api/src/controller/jobs.controller.ts
@@ -6,42 +6,88 @@ import iconv from 'iconv-lite'
 type Job = {
   url?: string
   title?: string
-  description?: string
+  descriptionEN?: string
+  descriptionPT?: string
+  noticeEN?: string
+  noticePT?: string
+  startDate?: string
+  endDate?: string
 }
 
-async function get(req: Request, res: Response) {
+const rootURL = 'https://sigarra.up.pt/feup/pt/'
+
+async function getHTML(url: string) {
+  const response = await axios.request({
+    method: 'GET',
+    url: url,
+    responseType: 'arraybuffer',
+    responseEncoding: 'binary',
+  })
+
+  return iconv.decode(response.data, 'iso-8859-15')
+}
+
+async function scrapeIndividualJob(url: string) {
+  const html = await getHTML(url)
+  const $ = cheerio.load(html)
+
+  const res: Array<string> = []
+  const targetUl = $('#conteudoinner')
+
+  const dates = $(targetUl).find('h2').text().split(' ')
+
+  $(targetUl)
+    .find('p')
+    .each((_, e) => {
+      const url = rootURL + $(e).find('a').attr('href')
+      const description = $(e).text()
+
+      res.push(description, url)
+    })
+
+  res.push(dates[2], dates[4])
+
+  return res
+}
+
+async function get(_req: Request, res: Response) {
   const jobs: Job[] = []
+  const rootURL = 'https://sigarra.up.pt/feup/pt/'
 
-  axios
-    .request({
-      method: 'GET',
-      url: 'https://sigarra.up.pt/feup/pt/noticias_geral.lista_noticias?p_grupo_noticias=19',
-      responseType: 'arraybuffer',
-      responseEncoding: 'binary',
-    })
-    .then(response => {
-      const html = iconv.decode(response.data, 'iso-8859-15')
-      const $ = cheerio.load(html)
-      const targetUl = $('#conteudoinner ul')
-      $(targetUl)
-        .find('li')
-        .each((i, e) => {
-          const url = $(e).find('span.titulo a').attr('href')
-          const title = $(e).find('span.titulo a').text()
-          const description = $(e).find('p.textopequenonoticia').text().split(' [ Ler toda a')[0]
+  const html = await getHTML(
+    `${rootURL}noticias_geral.lista_noticias?p_grupo_noticias=19`
+  )
+  const $ = cheerio.load(html)
+  const targetUl = $('#conteudoinner ul')
+  $(targetUl)
+    .find('li')
+    .each((_idx, e) => {
+      const url = rootURL + $(e).find('span.titulo a').attr('href')
+      const title = $(e).find('span.titulo a').text()
 
-          jobs.push({
-            url: url,
-            title: title,
-            description: description,
-          })
-        })
+      jobs.push({
+        url: url,
+        title: title,
+      })
+    })
 
-      res.send(jobs)
-    })
-    .catch(function (e) {
-      console.log(e)
-    })
+  const promises = jobs.map(job => scrapeIndividualJob(job.url!!))
+
+  const results = await Promise.all(promises)
+
+  for (const i in results) {
+    const job = jobs[i]
+    const result = results[i]
+
+    job.descriptionEN = result[0]
+    job.noticeEN = result[1]
+    job.descriptionPT = result[2]
+    job.noticePT = result[3]
+    job.startDate = result[4]
+    job.endDate = result[5]
+  }
+
+  res.send(jobs)
 }
 
 export default {
