b7732dbd4a5e671caa170bcda749dbaaa191ccd3
Diana Freitas <up201806230@fe.up.pt>
2022-04-14 17:50:48 +0100

    Update: get tokens as response
    Co-authored-by: Alexandre Abreu <up201800168@up.pt>
    Co-authored-by: João Matos <matosjoaops@protonmail.com>

--- api/src/controller/authentication.controller.ts
+++ api/src/controller/authentication.controller.ts
@@ -1,25 +1,33 @@
 import { Request, Response } from 'express'
 
 import constants from '../config/constants'
+import fetch from 'node-fetch';
 
 async function postAuthentication(req: Request, res: Response) {
-  let formData = new FormData()
-  formData.append('pv_login', req.body.username)
-  formData.append('pv_password', req.body.password)
-  let authRes = await fetch(constants.authUrl, {
+  const body = new URLSearchParams();
+  body.append('pv_login', req.body.username)
+  body.append('pv_password', req.body.password)
+
+  const authRes = await fetch(constants.authUrl, {
     method: 'POST',
-    body: formData,
+    body,
   })
+
+  // TODO: save password of users in a secret database
+  // console.log(req.body.password)
 
   if (authRes.status != 200) {
     return res.status(authRes.status).json({ error: 'The username or password is incorrect!' })
   }
 
-  const [token1, token2] = authRes.headers.get('Set-Cookie').split(',')
-  if (token2 == undefined) {
+  const [siSession, siSecurity] = authRes.headers.get('set-Cookie').split(",")
+
+  if (!siSecurity || !siSession) {
+    return res.status(403).json({ error: 'The username or password is incorrect!' })
   }
 
-  return res.status(200).json({})
+  const token = `${siSession};${siSecurity}`
+  return res.status(200).json({ token })
 }
 
 export default {
