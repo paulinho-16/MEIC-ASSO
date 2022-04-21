0bc9b8db036303f244b80a757c9ba0f7af059b1a
mariaaqbaia <61192386+mariaaqbaia@users.noreply.github.com>
2022-04-21 22:56:00 +0100

    endpoint to send notification

--- api/src/controller/notifications.controller.ts
+++ api/src/controller/notifications.controller.ts
@@ -1,8 +1,35 @@
 import { Request, Response } from 'express'
 import {v4 as uuidv4} from 'uuid';
+import fetch from 'node-fetch';
 
 async function createNotification(req: Request, res: Response) {
-    res.send('Create Notifications!')
+
+  const { client } = req.params
+    
+  const response = await fetch('https://fcm.googleapis.com/fcm/send', {
+    method: 'POST',
+    body: JSON.stringify({
+          "to" : "DEVICE_REGISTRATION_TOKEN",
+          "notification" : {
+              "body" : "Notification Body",
+              "title": "Notification Title"
+          },
+          "data" : {
+              "body" : "Notification Body",
+              "title": "Notification Title"
+          }
+    }),
+    headers: {
+      ContentType: 'application/json',
+      Authorization: 'key = AAAArP_sy-s:APA91bFbcwvy_mjJtt94nZZ9rd7CDWNI2wykQ_t9hYQejRVj7IkN2VyRor1ZGM-p8jx5VoU_Uuwgk22fsWhMaixcUIw3JaNpmgdzxtJXxnACIxc8TFzhiAXiimlLjq-TwDngrman-G3f'
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error! status: ${response.status}`);
+  }
+
+  return;
 }
 
 async function updateNotification(req: Request, res: Response) {
