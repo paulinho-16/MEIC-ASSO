238f690f9b9b9fa43f2f9cb294188ebf56758f82
RitaPeixoto <rita.peixoto2000@gmail.com>
2022-05-10 15:13:03 +0100

    swagger

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,14 @@
 import 'module-alias/register'
 import 'source-map-support/register'
+import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from'swagger-ui-express'
+import 'body-parser'
 import express from 'express'
 import routes from '@/routes'
 
 const app = express()
 const port = process.env.PORT || 3000
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +16,36 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
   console.log(`Application running in port ${port}.`)
 })
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Uni4all API',
+      version: '1.0.0',
+      description:
+        'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+      license: {
+        name: 'Licensed Under MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+      },
+      contact: {
+        name: 'JSONPlaceholder',
+        url: 'https://jsonplaceholder.typicode.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/associations', routes.associations)
 app.use('/library', routes.library)
--- api/src/routes/status.ts
+++ api/src/routes/status.ts
@@ -3,7 +3,14 @@ import express from 'express'
 import controller from '@/controller/status.controller'
 
 const router = express.Router()
+// routes/status.ts
 
+/**
+ * @swagger
+ * /status:
+ *  get:
+ *    summary:Get server uptime
+*/
 router.get('/', controller.get)
 
 export default router
