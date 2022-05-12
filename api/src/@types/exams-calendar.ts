cdebc82851dd7516a9a6e59ee67e77d06a106ed0
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2022-05-12 18:11:11 +0100

    done: season of the exam; missing: day and time

--- api/src/@types/exams-calendar.ts
+++ api/src/@types/exams-calendar.ts
@@ -1,11 +1,16 @@
 export type Exam = {
     acronym: string
+    day?: string
     time?: string
     rooms?: string
   }
 
+export type Season = {
+  name: string
+  exams: Exam[]
+}
 export type ExamsCalendar = {
   course: string
-  exams: Exam[]
+  seasons?: Season[]
 
 }
--- api/src/services/exams-calendar.ts
+++ api/src/services/exams-calendar.ts
@@ -3,9 +3,32 @@ import axios from 'axios'
 import constants from '@/config/constants'
 import {
 	Exam,
-	ExamsCalendar
+	ExamsCalendar, 
+	Season
 } from '@/@types/exams-calendar'
 
+function getTime($: cheerio.CheerioAPI, target: cheerio.Cheerio<cheerio.Element>) {
+	let text = ''
+	
+	
+	return text
+  }
+
+  
+function getExamsFromSeason($: cheerio.CheerioAPI, target: cheerio.Cheerio<cheerio.Element>){
+	const exams: Exam[] = []
+	const examsTarget = target.find('td.exame')
+	examsTarget.each((i, e) => {		
+		const exam: Exam = {			
+			acronym: $($(e).find('a')[0]).text(),
+			day: "",
+			time: "",
+			rooms: $($(e).find('span.exame-sala')[0]).text(),
+		}
+		exams.push(exam)
+	}) 
+	return exams
+}
 async function getExamsCalendar(courseID: string) {
     const examsCalendarUrl = `${constants.examsCalendarUrl}?p_curso_id=${courseID}`
 	console.log(examsCalendarUrl)
@@ -16,28 +39,27 @@ async function getExamsCalendar(courseID: string) {
 
 			// Get Targets
 			const courseTarget = $('a[name=ancora-conteudo] + h1 + h2')
-			const examsTarget = $('td.exame') // not working
+			const seasonsTarget = $('h3')
 
 			// Get Name
 			const course = $(courseTarget).text()
 
-			// Get Exams
-			const exams: Exam[] = []
-			examsTarget.each((i, e) => {
-				const exam: Exam = {
-					acronym: $($(e).find('a')[0]).text(),
-					time: "",
-					rooms: $($(e).find('span.exame-sala')[0]).text(),
+			// Get Season
+			const seasons: Season[] = []
+			seasonsTarget.each((i, e) => {
+				const season: Season = {
+					name: $($(e)).text(),
+					exams: getExamsFromSeason($, $(e).next().find('table').slice(1))
 				}
-				exams.push(exam)
+				seasons.push(season)
 			})
 			
-
 			// Get Exams Calendar
 			const examsCalendar: ExamsCalendar = {
 				course: course,
-				exams: exams
+				seasons: seasons,
 			}
+
 			
 			return examsCalendar
 		})
