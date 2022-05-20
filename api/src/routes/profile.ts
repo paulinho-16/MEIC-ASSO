031720b517a759e0c251beb86d921fe655040468
DiogoOliveiraReis <diogo.reis@fe.up.pt>
2022-05-20 14:33:23 +0100

    Added swagger doc

--- api/src/routes/profile.ts
+++ api/src/routes/profile.ts
@@ -5,5 +5,36 @@ import controller from '@/controller/profile.controller'
 const router = express.Router()
 
 router.get('/:studentNumber', controller.get)
+/**
+ * @swagger
+ * /profile/{studentNumber}:
+ *   get:
+ *     description: Retrieve studentNumber profile
+ *     parameters:
+ *       - in: path
+ *         name: studentNumber
+ *         required: true
+ *         description: student number
+ *     responseBody:
+ *       description: Response
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               sex:
+ *                 type: string
+ *               birthday:
+ *                 type: string
+ *               nationality:
+ *                 type: string
+ *               number:
+ *                 type: integer
+ *               email:
+ *                 type: string
+ */
 
 export default router
