4cdfa47c72dcd105081b259629f7cf91806f07fd
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2022-05-22 22:06:48 +0100

    Swagger documentation for exams-calendar endpoint

--- api/src/routes/exams-calendar.ts
+++ api/src/routes/exams-calendar.ts
@@ -6,4 +6,41 @@ const router = express.Router()
 
 router.get('/:id', controller.get)
 
+/**
+ * @swagger
+ * /exams-calendar/{courseID}
+ *  get:
+ *      description: Retrieve courseID exams calendar
+ *      parameters:
+ *          - in: path
+ *            name: courseID
+ *            required: true
+ *            description: course ID
+ *      responseBody:
+ *          description: Response
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          course: string
+ *                          seasons:
+ *                              type: array
+ *                              items:
+ *                                  type: object
+ *                                  properties:
+ *                                      name: string
+ *                                      exams:
+ *                                          type: array
+ *                                          items:
+ *                                              type: object
+ *                                              properties:
+ *                                                  acronym: string
+ *                                                  url: string
+ *                                                  day: string
+ *                                                  begin: string
+ *                                                  duration: string
+ *                                                  rooms: string
+ */
 export default router
