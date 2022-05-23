1b0cebb8aa5545468eaed273b776d13e550a708e
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2022-05-23 17:23:58 +0100

    swagger documentation finally working
    http://localhost:8080/api-docs/#/default/get_exams_calendar__courseID_

--- api/src/routes/exams-calendar.ts
+++ api/src/routes/exams-calendar.ts
@@ -4,43 +4,53 @@ import controller from '@/controller/exams-calendar.controller'
 
 const router = express.Router()
 
-/* *
+/**
  * @swagger
- * /exams-calendar/{courseID}
- *  get:
- *      description: Retrieve courseID exams calendar
- *      parameters:
- *          - in: path
- *            name: courseID
- *            required: true
- *            description: course ID
- *      responseBody:
- *          description: Response
- *          required: true
- *          content:
- *              application/json:
- *                  schema:
- *                      type: object
- *                      properties:
- *                          course: string
- *                          seasons:
- *                              type: array
- *                              items:
- *                                  type: object
- *                                  properties:
- *                                      name: string
- *                                      exams:
- *                                          type: array
- *                                          items:
- *                                              type: object
- *                                              properties:
- *                                                  acronym: string
- *                                                  url: string
- *                                                  day: string
- *                                                  begin: string
- *                                                  duration: string
- *                                                  rooms: string
- */
+ * /exams-calendar/{courseID}:
+ *   get:
+ *     summary: Retrieve courseID exams calendar
+ *     parameters:
+ *       - in: path
+ *         name: courseID
+ *         required: true
+ *         description: course ID
+ *     responses:
+ *       200:
+ *         description: The request was made with a valid course ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 course: 
+ *                   type: string
+ *                 seasons:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       name: 
+ *                         type: string
+ *                       exams:
+ *                         type: array
+ *                         items:
+ *                           type: object
+ *                           properties:
+ *                             acronym: 
+ *                               type: string
+ *                             url: 
+ *                               type: string
+ *                             day: 
+ *                               type: string
+ *                             begin:
+ *                               type: string
+ *                             duration:
+ *                               type: string
+ *                             rooms:
+ *                               type: string
+ *       500:
+ *         description: Unexpected error
+*/
 
 router.get('/:id', controller.get)
 
