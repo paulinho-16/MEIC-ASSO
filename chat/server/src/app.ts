d7eab89d9e6de17011143599b592f600f815730a
Ricardo Cardoso <ricardofdc@live.com>
2022-05-24 17:46:15 +0100

    fix merge problems

--- chat/server/src/app.ts
+++ chat/server/src/app.ts
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import 'module-alias/register';
-
-import express from 'express';
-import { Server } from 'socket.io';
-import http from 'http';
-import cors from 'cors';
-import setRequests from '@/socket';
-import UserManager from '@/userManager';
-
-const app = express();
-app.use(cors());
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:5500',
-    methods: ['GET'],
-  },
-});
-
-const userManager = new UserManager();
-
-setRequests(io, userManager);
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
-=======
 import 'module-alias/register';
 
 import express from 'express';
@@ -50,4 +22,3 @@ setRequests(io);
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
->>>>>>> 705b0e4bc1db26a2e7fc9a8357956fb3c3dcaaa3
--- chat/server/src/socket.ts
+++ chat/server/src/socket.ts
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-import UserManager from "@/userManager";
-
-const setRequests = (io:any, userManager:UserManager) => {
-  io.on('connection', (socket:any) => {
-    console.log('a user connected')
-
-    socket.on('disconnect', () => {
-      console.log('user disconnected')
-
-      const user = userManager.getUserBySocketId(socket.id)
-      if (user) {
-        io.emit('notification', `${user} has left the chat`)
-      }
-
-      userManager.removeUserBySocketId(socket.id)
-    })
-
-    socket.on('username', (username:string) => {
-      console.log('username', username)
-      const last = userManager.addUser(username, socket.id)
-      if (last) {
-        io.emit('notification', `${last} has left the chat`)
-      }
-      io.emit('notification', `${username} has joined the chat`)
-    })
-
-    socket.on('chat message', (msg:string, to:string) => {
-      console.log('chat message', msg, to)
-      // get sender on username
-      const sender = userManager.getUserBySocketId(socket.id)
-
-      if (to) {
-        io.to(userManager.getUserByUsername(to)).emit('private message', sender, msg)
-      } else {
-        socket.broadcast.emit('chat message', sender, msg)
-      }
-    })
-  })
-}
-
-export default setRequests
-=======
 const setRequests = (io:any) => {
   io.on('connection', (socket:any) => {
     console.log('a user connected')
@@ -63,4 +20,3 @@ const setRequests = (io:any) => {
 }
 
 export default setRequests
->>>>>>> 705b0e4bc1db26a2e7fc9a8357956fb3c3dcaaa3
