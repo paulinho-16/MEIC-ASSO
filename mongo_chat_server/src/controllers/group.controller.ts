80aa72dccd69d3da984d7fe44283ec983f2657da
Luís Tavares <luistavares10@outlook.pt>
2022-06-04 03:56:08 +0100

    Add user addition and removal

--- mongo_chat_server/src/controllers/group.controller.ts
+++ mongo_chat_server/src/controllers/group.controller.ts
@@ -8,49 +8,60 @@ async function getAllGroups(req: Request, res: Response) {
 
 async function _getGroupById(id: string) {
   return await Group.findById(id)
-		.then((group: any) => {
-			if (!group) {
-				return { status: 404, error: `Group with id '${id}' not found.` }
-			} else {
-				return { status: 200, data: group }
-			}
-		})
-		.catch(() => {
-			return { status: 404, data: `Group with id '${id}' not found.` }
-		})
+    .then((group: any) => {
+      if (!group) {
+        return { status: 404, error: `Group with id '${id}' not found.` };
+      } else {
+        return { status: 200, data: group };
+      }
+    })
+    .catch(() => {
+      return { status: 404, data: `Group with id '${id}' not found.` };
+    });
 }
 
 async function getGroupById(req: Request, res: Response) {
-  const {id} = req.params;
-  if(id === undefined) return res.status(400).json("You need to specify the id argument.");
+  const { id } = req.params;
+  if (id === undefined)
+    return res.status(400).json("You need to specify the id argument.");
   const response = await _getGroupById(id);
   return res.status(response.status).json(response.data);
 }
 
 async function getGroupsByUser(req: Request, res: Response) {
-  const {up} = req.params;
-  if(up === undefined) return res.status(400).json("You need to specify the up argument.");
-  return res.status(200).json((await Group.find({ userNumbers: up})));
+  const { up } = req.params;
+  if (up === undefined)
+    return res.status(400).json("You need to specify the up argument.");
+  return res.status(200).json(await Group.find({ userNumbers: up }));
 }
 
 async function createGroup(req: Request, res: Response) {
-  const {name, userNumbers} = req.body;
-  if(name  === undefined || userNumbers === undefined) return res.status(400).json("You need to specify all arguments: name and userNumbers.");
-  const newGroup = await Group.create({name, userNumbers});
+  const { name, userNumbers } = req.body;
+  if (name === undefined || userNumbers === undefined)
+    return res
+      .status(400)
+      .json("You need to specify all arguments: name and userNumbers.");
+  const newGroup = await Group.create({ name, userNumbers });
   await newGroup.populate("users");
   return res.status(200).json(newGroup);
 }
 
 async function getGroupMessages(req: Request, res: Response) {
   // eslint-disable-next-line prefer-const
-  let {groupID, perPage, page} = req.body;
-  if(groupID === undefined || perPage === undefined || page === undefined) return res.status(400).json("You need to specify the group, perPage and page arguments.")
+  let { groupID, perPage, page } = req.body;
+  if (groupID === undefined || perPage === undefined || page === undefined)
+    return res
+      .status(400)
+      .json("You need to specify the group, perPage and page arguments.");
 
   perPage = Number(perPage);
   page = Number(page);
-  if(isNaN(perPage) || isNaN(page)) return res.status(400).json("Both perPage and page arguments need to be numbers.")
+  if (isNaN(perPage) || isNaN(page))
+    return res
+      .status(400)
+      .json("Both perPage and page arguments need to be numbers.");
 
-  const {status, data} = await _getGroupById(groupID);
+  const { status, data } = await _getGroupById(groupID);
 
   if (status >= 400) {
     return res.status(status).json(data);
@@ -58,7 +69,47 @@ async function getGroupMessages(req: Request, res: Response) {
 
   const group = new Group(data);
   const messages = group.messages;
-  return res.status(200).json(messages.slice(page * perPage, (page + 1) * perPage));
+  return res
+    .status(200)
+    .json(messages.slice(page * perPage, (page + 1) * perPage));
+}
+
+async function addToGroup(req: Request, res: Response) {
+  const { groupID, userNumber } = req.body;
+  if (groupID === undefined || userNumber === undefined)
+    return res
+      .status(400)
+      .json("You need to specify the groupID and userNumber arguments.");
+  const { status, data } = await _getGroupById(groupID);
+  if (status >= 200) {
+    return res.status(status).json(data);
+  }
+
+  const group = await Group.findById(groupID);
+
+  group.userNumbers.push(userNumber);
+
+  await group.save();
+  return res.status(200).json(group);
+}
+
+async function removeFromGroup(req: Request, res: Response) {
+  const { groupID, userNumber } = req.body;
+  if (groupID === undefined || userNumber === undefined)
+    return res
+      .status(400)
+      .json("You need to specify the groupID and userNumber arguments.");
+  const { status, data } = await _getGroupById(groupID);
+  if (status >= 200) {
+    return res.status(status).json(data);
+  }
+
+  const group = await Group.findById(groupID);
+
+  group.userNumbers = group.userNumbers.filter((user) => user !== userNumber);
+
+  await group.save();
+  return res.status(200).json(group);
 }
 
 export default {
@@ -66,5 +117,7 @@ export default {
   getGroupById,
   getGroupsByUser,
   createGroup,
-  getGroupMessages
+  getGroupMessages,
+  addToGroup,
+  removeFromGroup,
 };
