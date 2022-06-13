67ef50b0fdf9e1270c43b53f8d1daf9ccddb8d90
pedrojfs17 <up201806227@fe.up.pt>
2022-06-13 13:56:05 +0100

    Finished component

--- api/src/controller/payments.controller.ts
+++ api/src/controller/payments.controller.ts
@@ -1,11 +1,9 @@
 import { Request, Response } from 'express'
 
-import { paymentsPageHTML } from '../config/data'
-
 import paymentsService from '@/services/payments'
 
 async function get(req: Request, res: Response) {
-  const payments = await paymentsService.fetchPayments(paymentsPageHTML.toString())
+  const payments = await paymentsService.fetchPayments(req.body)
 
   return res.status(200).json(payments)
 }
